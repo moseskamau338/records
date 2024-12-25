d5ece63727ee47b510b7835cb1ce923ce469fcc7
Moses Kamau <moseskamau338@gmail.com>
2024-12-25 14:28:11 +0300

    feat: update design for project creation templates view

--- resources/js/Pages/Projects/CreateWithTemplate.vue
+++ resources/js/Pages/Projects/CreateWithTemplate.vue
@@ -1,29 +1,41 @@
 <script setup lang="ts">
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head, Link } from "@inertiajs/vue3";
-import { NButton, NDivider } from "naive-ui";
+import { Head } from "@inertiajs/vue3";
+import {NButton, NCard, NImage, NInput, NPopselect} from "naive-ui";
+import {ref} from "vue";
 import JIcon from "@/Components/App/JIcon.vue";
 
-const projects = [
-    { name: "Sample bank reconciliation", id: "76543567-87654" },
-    { name: "Transactions mapping", id: "76500937-87654" },
-    { name: "Charges report", id: "776543567-87654" },
-];
+const template_filter = ref('all')
+
 </script>
 
 <template>
     <Head title="Projects" />
 
     <AuthenticatedLayout>
-        <div class="flex flex-row items-center justify-between mt-4">
+        <div class="py-1">
+            <n-input placeholder="Search templates..." class="max-w-[400px]" />
+        </div>
 
-            <n-button type="primary" ghost size="small"
-                >+ Create project</n-button
-            >
+        <div class="p-2">
+            <n-popselect  v-model:value="template_filter" :options="[
+                {label:'All templates', value:'all'},
+                {label:'2-Way Reconciliation', value:'2-way'},
+                {label:'N-Way Reconciliation', value:'n-way'},
+            ]">
+                <n-button size="small" icon-placement="right">
+                    <template #icon>
+                        <j-icon name="material-symbols-light:keyboard-arrow-down" />
+                    </template>
+                    {{ template_filter }}</n-button>
+            </n-popselect>
         </div>
-        <div>
-         templates
-            {{$page.props.breadcrumbs}}
-        </div>
+        <button class="block text-left max-w-[250px] border-2 rounded-sm dark:border-neutral-700 hover:shadow-md transition-all hover:border-brand/60 dark:hover:border-brand/60">
+        <img src="/images/Bank-Recon.jpg" class="object-cover h-48 w-96"/>
+           <div class="p-2 prose">
+               <h4 class="font-bold text-md">Bank Account Reconciliation Template</h4>
+               <p class="text-neutral-500">Lorem ipsum dolor sit amet, consecte.</p>
+           </div>
+        </button>
     </AuthenticatedLayout>
 </template>
